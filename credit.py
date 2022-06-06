fe34448eef600e9f4da12d15b84dfc212ff9ba90
Jacob Muñoz <jmg_extreme@hotmail.com>
2022-06-06 18:19:12 -0500

    una variable menos

--- credit.py
+++ credit.py
@@ -9,28 +9,28 @@ class CreditCard():
             return self.check_credit_card_no(creditcard_no, type="AMEX")
         elif len(creditcard_no) == 13 or len(creditcard_no) == 16:
             return self.check_credit_card_no(creditcard_no, type="VISA")
+        else:
+            return self.check_credit_card_no(creditcard_no, type="VALID")
         
-
     def check_credit_card_no(self, creditcard_no, type):
-        """Se genera el metodo check_credit_card_no verifica si el numero de tarjeta es valido con el algoritmo de Luhn con funcino 121212121212"""
+        """Se genera el metodo check_credit_card_no verifica si el numero de tarjeta es valido con el algoritmo de Luhn"""
         processed_digits = []
-        creditcard_no_list=[]
         for digit in creditcard_no:
-            creditcard_no_list.append(int(digit))
+            creditcard_no=[]
+            creditcard_no.append(int(digit))
             double_digit =int(digit)*2
             if double_digit>9:
                 double_digit = double_digit-9 
                 processed_digits.append(double_digit)
             else:
                 processed_digits.append(int(double_digit))
-        total_1= sum(creditcard_no_list[::2]) + sum(processed_digits[1::2])
-        total_2= sum(creditcard_no_list[1::2]) + sum(processed_digits[::2])
+        total_1= sum(creditcard_no[::2]) + sum(processed_digits[1::2])
+        total_2= sum(creditcard_no[1::2]) + sum(processed_digits[::2])
         if total_1 % 10 == 0 or total_2 % 10 == 0:
             print(type) 
         else:
             print("INVALID")
     
-
 creditcard_no = input("Insert your credit card number: ").strip()
 CreditCard().check_credit_card_type(creditcard_no)   
 
