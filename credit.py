416b29eb1d87be465920a4eb868e5752ce62a347
Jacob Muñoz <jmg_extreme@hotmail.com>
2022-06-06 11:52:57 -0500

    first commit

--- credit.py
+++ credit.py
@@ -2,34 +2,43 @@ class CreditCard():
     """Se genera la clase CreditCard"""
     def check_credit_card_type(self, creditcard_no):
         """Se genera el metodo este metodo check_credit_card_type verifica el typo de tarjeta"""
+        type=""
         if int(creditcard_no[:2]) in [51, 52, 53, 54, 55 ] and len(creditcard_no) == 16:
-            return "MASTERCARD"
+            return self.check_credit_card_no(creditcard_no, type="MASTER CARD")
         elif int(creditcard_no[:2]) in [34, 37]:
-            return "AMEX "
+            return self.check_credit_card_no(creditcard_no, type="AMEX")
         elif len(creditcard_no) == 13 or len(creditcard_no) == 16:
-            return "VISA"
-        else:
-            return "VALID"
+            return self.check_credit_card_no(creditcard_no, type="VISA")
+        
 
-    def check_credit_card_no(self, creditcard_no):
+    def check_credit_card_no(self, creditcard_no, type):
         """Se genera el metodo check_credit_card_no verifica si el numero de tarjeta es valido con el algoritmo de Luhn"""
         processed_digits = []
         for index, digit in enumerate(creditcard_no):
-            if index % 2 != 0:
-                double_digit = int(digit)*2
-                if double_digit > 9:
-                    double_digit=double_digit-9
-                processed_digits.append(double_digit)
-            else:
-                processed_digits.append(int(digit))
+            if type == "VISA":
+                if index % 2 == 0:
+                    double_digit = int(digit)*2
+                    if double_digit > 9:
+                        double_digit=double_digit-9
+                    processed_digits.append(double_digit)
+                else:
+                    processed_digits.append(int(digit))
+            elif type == "MASTER CARD" or "AMEX"  :
+                if index % 2 != 0:
+                    double_digit = int(digit)*2
+                    if double_digit > 9:
+                        double_digit=double_digit-9
+                    processed_digits.append(double_digit)
+                else:
+                    processed_digits.append(int(digit))
         total = sum(processed_digits)
         if total % 10 == 0:
-            print(self.check_credit_card_type(creditcard_no)) 
+            print(type) 
         else:
             print("INVALID")
 
 creditcard_no = input("Insert your credit card number: ").strip()
-CreditCard().check_credit_card_no(creditcard_no)   
+CreditCard().check_credit_card_type(creditcard_no)   
 
     
 
